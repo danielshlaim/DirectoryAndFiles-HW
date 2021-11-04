226b2ac4d16cf7ab6e5c7312dd2f1baf3427f20b
maorx <maorx@DESKTOP-P40Q1AV>
2021-11-04 23:55:50 +0200

    Add project files.

---
+++ DirectoryAndFiles HW/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DirectoryAndFiles_HW
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //string[] folders = System.IO.Directory.GetDirectories(@"C:\", "*", System.IO.SearchOption.TopDirectoryOnly);
+            //for (int i = 0; i < folders.Length; i++)
+            //{
+            //    Console.WriteLine(folders[i]);
+            //}
+
+            //File.Create("D:\\ DanielTest\\a.csv");
+            //foreach (var drive in drives)
+            //{
+            //    Console.WriteLine(drive);
+            //}
+            var drives = Directory.GetLogicalDrives();
+           
+            var topDirectories = Directory.GetDirectories(drives[0]);
+            foreach (var dir in topDirectories.Take(10))
+            {
+                Console.WriteLine(dir);
+            }
+
+            Console.WriteLine("=============================================");
+
+            printGreatest3Files(@"c:\");
+            Console.WriteLine("=============================================");
+            Student.StudentToJason();
+            Console.WriteLine("=============================================");
+            var a = Student.StudentFixedL();
+            Console.WriteLine(a);
+            Console.WriteLine("=============================================");
+            Student.StudentCsvL();
+            Console.WriteLine("=============================================");
+            Student.ReadStudentListFromScv();
+
+            #region ex 7 + ex 8 + ex9
+            // ex 7 = fixed length
+            // ex 8  = הוא מגביל אותך
+            // ex 9 = קובץ טקסט רגיל יכול להכיל רק אותיות ומספרים בקוד האסקי וקובץ בינארי יכול להכיל את כל התווים האפשרים
+
+            #endregion
+        }
+        public static void printGreatest3Files(string dir)
+        {
+            var di = new DirectoryInfo(dir);
+            var result = di.GetFiles().OrderByDescending(file => file.Length).Take(3).ToList();
+            foreach (var fileInfo in result)
+            {
+                Console.WriteLine(fileInfo.Name + "last updated: " + fileInfo.LastWriteTime);
+            }
+        }
+
+    }
+    
+}
---
+++ DirectoryAndFiles HW/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace DirectoryAndFiles_HW
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+      static  List<Student> studentlist = new List<Student>();
+
+        static Student()
+        {
+            studentlist.Add(new Student{FirstName = "daniel", LastName = "shlaim"});
+            studentlist.Add(new Student { FirstName = "avichii", LastName = "Dahan" });
+            studentlist.Add(new Student{FirstName = "gil", LastName = "alkobi"});
+            studentlist.Add(new Student { FirstName = "maor", LastName = "deri" });           
+        }
+
+        public static void StudentToJason()
+        {
+            var x = JsonSerializer.Serialize(studentlist);
+            File.WriteAllText(@"C:\Users\maorx\OneDrive\Desktop\daniel folder\DirectoryAndFiles HW\DirectoryAndFiles HW\x.json", x);
+        }
+
+        public  string StudentFixed()
+        {
+            string s  = string.Format("{0:5} {1:6}",FirstName,LastName);
+            return s;
+        }
+        public static string StudentFixedL()
+        {
+            string a = "";
+            foreach (var item in studentlist)
+            {
+
+                a += item.StudentFixed() + Environment.NewLine;
+            }
+            return a;
+        }
+
+        public string StudentScv()
+        {
+            string v = string.Format("FirstName :{0} LastName :{1}", FirstName, LastName);
+            return v;
+        }
+        public static void StudentCsvL()
+        {
+            string a = "";
+            foreach (var item in studentlist)
+            {
+
+                a += item.StudentFixed() + Environment.NewLine;
+            }
+            File.WriteAllText(@"C:\Users\maorx\OneDrive\Desktop\daniel folder\DirectoryAndFiles HW\DirectoryAndFiles HW\y.Csv", a);
+            
+        }
+
+        public static void ReadStudentListFromScv()
+        {
+            var stu = File.ReadAllLines(@"C:\Users\maorx\OneDrive\Desktop\daniel folder\DirectoryAndFiles HW\DirectoryAndFiles HW\y.Csv").ToList();
+        }
+
+
+    }
+}
